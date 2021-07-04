38501e62ea3091bb7b29f7522cb3c20f0406ff21
Alexi Peck <alexinicolaspeck@gmail.com>
2021-07-04 17:16:52 +1000

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -415,16 +415,18 @@ fn main() {
 
     for content in queue.main_queue {
         let source = format!("{}{}", content.parent_directory, content.filename);
-        let target = format!("{}_h265.mp4", content.filename_woe);
-        println!("Starting encode of {}\nEncoding to {}_h265.mp4", content.filename, content.filename_woe);
+        println!("{}", source);
+        let target = format!("{}_encode.mp4", content.filename_woe);
+        println!("{}", target);
+        println!("Starting encode of {}\nEncoding to {}_encode.mp4", content.filename, content.filename_woe);
         //async
         //let encode_string: Vec<&str> = vec!["-i", &source, "-c:v", "libx265", "-crf", "25", "-preset", "slower", "-profile:v", "main", "-c:a", "aac", "-q:a", "224k", &target];
         
         //let encode_string: String = format!("ffmpeg -i \"{}\" -c:v libx265 -crf 25 -preset slower -profile:v main -c:a aac -q:a 224k \"{}\"", source, target);
         //println!("Source: {}\nTarget: {}\nEncode string: {}", source, target, encode_string);
-        let output = encode(&source, &target);
-        println!("{}", output);
-        rename(&content.parent_directory, &content.filename, &target);
+        //let output = encode(&source, &target);
+        //println!("{}", output);
+        //rename(&content.parent_directory, &content.filename, &target);
     }
 
     if false {
