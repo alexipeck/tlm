f8e2d69ee8d0bbeaa1a8c12862a0b9e1a8d19411
Alexi Peck <alexinicolaspeck@gmail.com>
2021-09-19 19:36:01 +1000

    Remove unused imports

--- src/command_controller.rs
+++ src/command_controller.rs
@@ -1,8 +1,3 @@
-use std::{collections::VecDeque, sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}}, thread};
-use tracing::{Level, event};
-
-use crate::{config::{Config, Preferences}, scheduler::{Hash, ImportFiles, ProcessNewFiles, Scheduler, Task, TaskType}};
-
 pub struct CommandController {
     
 }
@@ -10,7 +5,7 @@ pub struct CommandController {
 impl CommandController {
     pub fn new() -> Self {
         CommandController {
-            
+
         }
     }
 
