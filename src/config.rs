alexipeck/tlm

commit b6b406e9be735d410df24587232a5dc770b55f79

Fix hashing output, parallelise creating generics, remove websockets
Refactor the hashing output

Remove Websockets as it was causing too many issues

The websockets crate contains 2 vulnerabilities which
I lack the time to fix. As well as this windows appears
to be stopping websockets from working. Looking into
replacing this with a more up to date library. Or using
hyper/similar directly

Use rayon to parallelise the creation of generics

Because we spend a huge amount of time generating the profile
data for generics we can do it in parallel. This works well because
from testing roughly 60% of the time is spent waiting for mediainfo
to start

Correctly track remaining hashes to be calculated

Cargo.toml
@@ -17,7 +17,7 @@ seahash = "4.1.0"
 lazy_static = "1.4.0"
 serde_json = "1.0.67"
 ctrlc = "3.2.0"
-websocket = "0.26.2"
 tracing-appender = "0.1.2"
 tracing-subscriber = "0.2.21"
 tracing = "0.1.26"
+rayon = "1.5.1"

src/main.rs
@@ -1,12 +1,9 @@
 extern crate diesel;
-extern crate websocket;
 
 use tlm::{
     config::{Config, Preferences},
     scheduler::{Hash, ImportFiles, ProcessNewFiles, Scheduler, Task, TaskType},
 };
-use websocket::sync::Server;
-use websocket::OwnedMessage;
 
 use std::collections::VecDeque;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -19,6 +16,9 @@ use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::registry::Registry;
 
 fn main() {
+    //Optimal seems to be 2x the number of threads but more testing required
+    //By default this is the number of threads the cpu has
+    //rayon::ThreadPoolBuilder::new().num_threads(4).build_global().unwrap();
     let file = tracing_appender::rolling::daily("./logs", "tlm.log");
     let (writer, _guard) = tracing_appender::non_blocking(stdout());
     let (writer2, _guard) = tracing_appender::non_blocking(file);
@@ -39,8 +39,7 @@ fn main() {
     let tasks: Arc<Mutex<VecDeque<Task>>> = Arc::new(Mutex::new(VecDeque::new()));
 
     let stop_scheduler = Arc::new(AtomicBool::new(false));
-    let mut scheduler: Scheduler =
-        Scheduler::new(config.clone(), tasks.clone(), stop_scheduler.clone());
+    let mut scheduler: Scheduler = Scheduler::new(config, tasks.clone(), stop_scheduler.clone());
 
     let inner_pref = preferences.clone();
     //Start the scheduler in it's own thread and return the scheduler at the end
@@ -52,7 +51,7 @@ fn main() {
 
     //Initial setup in own scope so lock drops
     {
-        let mut tasks_guard = tasks.lock().unwrap();
+        let mut tasks_guard = tasks.lock().unwrap(); //TODO: Switch to a fair mutex implementation
         tasks_guard.push_back(Task::new(TaskType::Hash(Hash::default())));
 
         tasks_guard.push_back(Task::new(TaskType::ImportFiles(ImportFiles::default())));
@@ -60,12 +59,6 @@ fn main() {
         tasks_guard.push_back(Task::new(TaskType::ProcessNewFiles(
             ProcessNewFiles::default(),
         )));
-    }
-
-    let mut server = Server::bind("127.0.0.1:49200").unwrap();
-    if server.set_nonblocking(true).is_err() {
-        event!(Level::ERROR, "");
-        panic!();
     }
 
     if !preferences.disable_input {
@@ -76,19 +69,7 @@ fn main() {
             running_inner.store(false, Ordering::SeqCst)
         })
         .expect("Error setting Ctrl-C handler");
-        while running.load(Ordering::SeqCst) {
-            if let Ok(wsupgrade) = server.accept() {
-                let message = wsupgrade.accept().unwrap().recv_message().unwrap();
-                match message {
-                    OwnedMessage::Text(text) => {
-                        println!("{}", text);
-                    }
-                    _ => {
-                        println!("Unk");
-                    }
-                }
-            }
-        }
+        while running.load(Ordering::SeqCst) {}
     }
 
     stop_scheduler.store(true, Ordering::Relaxed);
@@ -101,5 +82,5 @@ fn main() {
     scheduler.file_manager.print_shows(&preferences);
     scheduler.file_manager.print_generics(&preferences);
     scheduler.file_manager.print_episodes(&preferences);
-    scheduler.file_manager.print_rejected_files(); //I'm all for it as soon as it's disabled by default
+    //scheduler.file_manager.print_rejected_files(); //I'm all for it as soon as it's disabled by default
 }

src/manager.rs
@@ -10,6 +10,7 @@ use crate::{
 };
 use diesel::pg::PgConnection;
 use lazy_static::lazy_static;
+use rayon::prelude::*;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashSet, fmt, path::PathBuf};
@@ -172,30 +173,33 @@ impl FileManager {
         let connection = establish_connection();
         let mut new_episodes = Vec::new();
         let mut new_generics = Vec::new();
-        let mut temp_generics = Vec::new();
 
         lazy_static! {
             static ref REGEX: Regex = Regex::new(r"S[0-9]*E[0-9\-]*").unwrap();
         }
 
         //Create Content and NewContent that will be added to the database in a batch
-        while !self.new_files_queue.is_empty() {
-            let current = self.new_files_queue.pop();
-            if let Some(current) = current {
-                let mut generic = Generic::new(&current);
+        let mut temp_generics: Vec<Generic> = self
+            .new_files_queue
+            .par_iter()
+            .map(|current| {
+                let mut generic = Generic::new(current);
 
                 //TODO: Why yes this is slower, no I don't care about 100ms right now
                 match REGEX.find(&generic.get_filename()) {
                     None => {}
                     Some(_) => generic.designation = Designation::Episode,
                 }
-                new_generics.push(NewGeneric::new(
-                    String::from(generic.full_path.to_str().unwrap()),
-                    generic.designation as i32,
-                    generic.profile,
-                ));
-                temp_generics.push(generic);
-            }
+
+                generic
+            })
+            .collect();
+        for generic in &temp_generics {
+            new_generics.push(NewGeneric::new(
+                String::from(generic.full_path.to_str().unwrap()),
+                generic.designation as i32,
+                generic.profile,
+            ));
         }
 
         //Insert the generic and then update the uid's for the full Generic structure

src/scheduler.rs
@@ -124,13 +124,23 @@ impl Hash {
         let is_finished_inner = is_finished.clone();
         //Hash files until all other functions are complete
         let handle = Some(thread::spawn(move || {
+            let mut current_content = current_content;
+            current_content.retain(|elem| elem.hash.is_none());
+            let length = current_content.len();
             let connection = establish_connection();
             let mut did_finish = true;
-            for mut content in current_content {
+            for (i, content) in current_content.iter_mut().enumerate() {
                 if content.hash.is_none() {
                     content.hash();
+                    event![
+                        Level::DEBUG,
+                        "Hashed[{} of {}]: {}",
+                        i + 1,
+                        length,
+                        content.to_string()
+                    ];
                     content.fast_hash();
-                    if GenericModel::from_generic(content)
+                    if GenericModel::from_generic(content.clone())
                         .save_changes::<GenericModel>(&connection)
                         .is_err()
                     {
@@ -284,7 +294,7 @@ impl Scheduler {
                 handles.remove(i);
             }
             {
-                let mut tasks = self.tasks.lock().unwrap();
+                let mut tasks = self.tasks.lock().unwrap(); //TODO: Switch to a fair mutex implementaion
 
                 //When the queue is empty we wait until another item is added or user input is marked as completed
                 if tasks.len() == 0 {

.gitignore
@@ -8,3 +8,4 @@ workspace.code-workspace
 *.data.old
 .vscode
 logs/
+todo

src/database.rs
@@ -1,16 +1,9 @@
 use crate::schema::generic::designation;
 use crate::{
-    designation::Designation,
-    generic::Generic,
-    model::*,
-    schema::episode as episode_table,
-    schema::episode::dsl::episode as episode_db,
-    schema::generic as generic_table,
-    schema::generic::dsl::generic as generic_data,
-    schema::show as show_table,
-    schema::show::dsl::show as show_db,
-    show::Episode,
-    show::Show,
+    designation::Designation, generic::Generic, model::*, schema::episode as episode_table,
+    schema::episode::dsl::episode as episode_db, schema::generic as generic_table,
+    schema::generic::dsl::generic as generic_data, schema::show as show_table,
+    schema::show::dsl::show as show_db, show::Episode, show::Show,
 };
 use diesel::{pg::PgConnection, prelude::*};
 use std::env;

src/generic.rs
@@ -6,6 +6,8 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use std::fmt;
+
 use tracing::{event, Level};
 
 use crate::config::Preferences;
@@ -25,6 +27,12 @@ pub struct Generic {
     pub hash: Option<String>,
     pub fast_hash: Option<String>,
     pub profile: Option<Profile>,
+}
+
+impl fmt::Display for Generic {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.get_full_path())
+    }
 }
 
 impl Generic {

commit 46a8a5619ad32a75bb593f9710df38584ce3a2c1

Reimplement websockets using tokio-tungstenite
The below were squashed and merged

Add a config setting for the port that the websocket will listen on
as well as a flag to overwrite the config

Handle failure to bind to one or both ports

Update some dependencies, turn off some default features

Listen for websockets on ipv4 and ipv6

After looking into it tokio-tungstenite seems like the
easier choice and has a ctrl-c listener built in

Add ability to add tasks via web socket, fix regression in manager

The web socket thread now acceps the commands process, import, and hash
they add the corresponding task to the end of the queue as a
proof of concept.

The regression was caused by me in b6b406e9be735d410df24587232a5dc770b55f79
making process_new files parallel meant that I switched a for while loop
that popped elements to a for loop. This meant that new_files_queue
was no longer being emptied and duplicates would be created if you
processed files more than one. This is fixed by simply clearing
new_files_queue after the for loop

Cargo.toml
@@ -8,16 +8,22 @@ edition = "2018"
 
 [dependencies]
 walkdir = "2"
-regex = "1"
+regex = { version = "1", default-features = false }
 argparse = "0.2.2"
 serde = { version = "1.0", features = ["derive"] }
 toml = "0.5.8"
-diesel = { version = "1.4.7", features = ["postgres"] }
+diesel = { version = "1.4.7", default-features = false, features = ["postgres"] }
 seahash = "4.1.0"
 lazy_static = "1.4.0"
-serde_json = "1.0.67"
-ctrlc = "3.2.0"
+serde_json = { version = "1.0.67", default-features = false }
 tracing-appender = "0.1.2"
-tracing-subscriber = "0.2.21"
+tracing-subscriber = { version = "0.2.24" }
 tracing = "0.1.26"
 rayon = "1.5.1"
+
+#Web sockets related
+tokio-tungstenite = {version="0.15.0"}
+tokio = { version = "1.0.0", default-features = false, features = ["macros", "net", "rt-multi-thread", "time", "signal"] }
+futures-channel = "0.3"
+url = "2.0.0"
+futures-util = { version = "0.3", default-features = false, features = ["async-await"] }

src/config.rs
@@ -1,7 +1,7 @@
 //!Set of functions and structures to make is easier to handle the config file
 //!and command line arguments
 use crate::manager::TrackedDirectories;
-use argparse::{ArgumentParser, Store, StoreFalse, StoreTrue};
+use argparse::{ArgumentParser, Store, StoreFalse, StoreOption, StoreTrue};
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
@@ -15,13 +15,14 @@ pub struct Config {
     pub allowed_extensions: Vec<String>,
     pub ignored_paths: Vec<String>,
     pub tracked_directories: TrackedDirectories,
+    pub port: u16,
 }
 
 impl Config {
     ///Config constructor loads the config from the path defined at the cli
     /// or if it doesn't exist creates a default config file
     pub fn new(preferences: &Preferences) -> Config {
-        let config: Config;
+        let mut config: Config;
 
         if Path::new(&preferences.config_file_path).exists() {
             let config_toml = match fs::read_to_string(&preferences.config_file_path) {
@@ -53,12 +54,17 @@ impl Config {
                 allowed_extensions,
                 ignored_paths,
                 tracked_directories,
+                port: 8888,
             };
             let toml = toml::to_string(&config).unwrap();
             if fs::write(&preferences.config_file_path, toml).is_err() {
                 event!(Level::ERROR, "Failed to write config file");
                 panic!();
             }
+        }
+
+        if preferences.port.is_some() {
+            config.port = preferences.port.unwrap();
         }
 
         config
@@ -76,6 +82,7 @@ pub struct Preferences {
     pub config_file_path: String,
     pub timing_enabled: bool,
     pub timing_threshold: u128,
+    pub port: Option<u16>,
     pub generic_output_whitelisted: bool,
     pub show_output_whitelisted: bool,
     pub episode_output_whitelisted: bool,
@@ -92,6 +99,7 @@ impl Default for Preferences {
             config_file_path: String::from("./.tlm_config"),
             timing_enabled: false,
             timing_threshold: 0,
+            port: None,
 
             generic_output_whitelisted: false,
             show_output_whitelisted: false,
@@ -175,6 +183,12 @@ impl Preferences {
             &["--disable-input", "--no-input"],
             StoreTrue,
             "Don't accept any inputs from the user (Testing only will be removed later)",
+        );
+
+        parser.refer(&mut self.port).add_option(
+            &["--port", "-p"],
+            StoreOption,
+            "Overwrite the port set in the config",
         );
 
         parser.parse_args_or_exit();

src/lib.rs
@@ -9,6 +9,7 @@ pub mod profile;
 pub mod scheduler;
 pub mod schema;
 pub mod show;
+pub mod ws;
 
 #[macro_use]
 extern crate diesel;

src/main.rs
@@ -3,9 +3,11 @@ extern crate diesel;
 use tlm::{
     config::{Config, Preferences},
     scheduler::{Hash, ImportFiles, ProcessNewFiles, Scheduler, Task, TaskType},
+    ws::run,
 };
 
 use std::collections::VecDeque;
+use std::io::Error as IoError;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -15,7 +17,8 @@ use tracing::{event, Level};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::registry::Registry;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), IoError> {
     //Optimal seems to be 2x the number of threads but more testing required
     //By default this is the number of threads the cpu has
     //rayon::ThreadPoolBuilder::new().num_threads(4).build_global().unwrap();
@@ -39,7 +42,8 @@ fn main() {
     let tasks: Arc<Mutex<VecDeque<Task>>> = Arc::new(Mutex::new(VecDeque::new()));
 
     let stop_scheduler = Arc::new(AtomicBool::new(false));
-    let mut scheduler: Scheduler = Scheduler::new(config, tasks.clone(), stop_scheduler.clone());
+    let mut scheduler: Scheduler =
+        Scheduler::new(config.clone(), tasks.clone(), stop_scheduler.clone());
 
     let inner_pref = preferences.clone();
     //Start the scheduler in it's own thread and return the scheduler at the end
@@ -62,14 +66,7 @@ fn main() {
     }
 
     if !preferences.disable_input {
-        let running = Arc::new(AtomicBool::new(true));
-        let running_inner = running.clone();
-        ctrlc::set_handler(move || {
-            event!(Level::WARN, "Stop signal received shutting down");
-            running_inner.store(false, Ordering::SeqCst)
-        })
-        .expect("Error setting Ctrl-C handler");
-        while running.load(Ordering::SeqCst) {}
+        run(config.port, tasks).await?;
     }
 
     stop_scheduler.store(true, Ordering::Relaxed);
@@ -83,4 +80,5 @@ fn main() {
     scheduler.file_manager.print_generics(&preferences);
     scheduler.file_manager.print_episodes(&preferences);
     //scheduler.file_manager.print_rejected_files(); //I'm all for it as soon as it's disabled by default
+    Ok(())
 }

src/manager.rs
@@ -194,6 +194,7 @@ impl FileManager {
                 generic
             })
             .collect();
+        self.new_files_queue.clear();
         for generic in &temp_generics {
             new_generics.push(NewGeneric::new(
                 String::from(generic.full_path.to_str().unwrap()),

src/scheduler.rs
@@ -146,7 +146,7 @@ impl Hash {
                         .save_changes::<GenericModel>(&connection)
                         .is_err()
                     {
-                        eprintln!("Failed to update hash in database");
+                        event!(Level::ERROR, "Failed to update hash in database");
                     }
                 }
                 if is_finished_inner.load(Ordering::Relaxed) {

src/schema.rs
@@ -55,10 +55,4 @@ joinable!(episode -> generic (generic_uid));
 joinable!(episode -> show (show_uid));
 joinable!(job_task_queue -> job_queue (job_uid));
 
-allow_tables_to_appear_in_same_query!(
-    episode,
-    generic,
-    job_queue,
-    job_task_queue,
-    show,
-);
+allow_tables_to_appear_in_same_query!(episode, generic, job_queue, job_task_queue, show,);

src/ws.rs
@@ -1,0 +1,173 @@
+//!Module for handing web socket connections that will be used with
+//!both the cli and web ui controller to communicate in both directions as necessary
+use std::collections::VecDeque;
+use tracing::{event, Level};
+
+use crate::scheduler::{Hash, ImportFiles, ProcessNewFiles, Task, TaskType};
+
+use std::{
+    collections::HashMap,
+    env,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
+
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::signal;
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+async fn handle_connection(
+    peer_map: PeerMap,
+    raw_stream: TcpStream,
+    addr: SocketAddr,
+    tasks: Arc<Mutex<VecDeque<Task>>>,
+) {
+    event!(Level::INFO, "Incoming TCP connection from: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(raw_stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+    event!(Level::INFO, "WebSocket connection established: {}", addr);
+
+    // Insert the write part of this peer to the peer map.
+    let (tx, rx) = unbounded();
+    peer_map.lock().unwrap().insert(addr, tx);
+
+    let (outgoing, incoming) = ws_stream.split();
+
+    let broadcast_incoming = incoming.try_for_each(|msg| {
+        let message = msg
+            .to_text()
+            .unwrap()
+            .strip_suffix("\r\n")
+            .or_else(|| msg.to_text().unwrap().strip_suffix('\n'))
+            .unwrap_or_else(|| msg.to_text().unwrap());
+        event!(Level::INFO, "Received a message from {}: {}", addr, message);
+
+        match message {
+            "hash" => {
+                tasks
+                    .lock()
+                    .unwrap()
+                    .push_back(Task::new(TaskType::Hash(Hash::default())));
+            }
+            "import" => tasks
+                .lock()
+                .unwrap()
+                .push_back(Task::new(TaskType::ImportFiles(ImportFiles::default()))),
+            "process" => tasks
+                .lock()
+                .unwrap()
+                .push_back(Task::new(TaskType::ProcessNewFiles(
+                    ProcessNewFiles::default(),
+                ))),
+            _ => event!(Level::WARN, "{} is not a valid input", message),
+        }
+
+        future::ok(())
+    });
+
+    let receive_from_others = rx.map(Ok).forward(outgoing);
+
+    pin_mut!(broadcast_incoming, receive_from_others);
+    future::select(broadcast_incoming, receive_from_others).await;
+
+    event!(Level::INFO, "{} disconnected", &addr);
+    peer_map.lock().unwrap().remove(&addr);
+}
+
+pub async fn run(port: u16, tasks: Arc<Mutex<VecDeque<Task>>>) -> Result<(), IoError> {
+    let addr_ipv4 = env::args()
+        .nth(1)
+        .unwrap_or_else(|| format!("127.0.0.1:{}", port));
+
+    let addr_ipv6 = env::args()
+        .nth(1)
+        .unwrap_or_else(|| format!("[::1]:{}", port));
+
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
+
+    // Create the event loop and TCP listener
+    let try_socket_ipv4 = TcpListener::bind(&addr_ipv4).await;
+    let listener_ipv4 = try_socket_ipv4;
+    let try_socket_ipv6 = TcpListener::bind(&addr_ipv6).await;
+    let listener_ipv6 = try_socket_ipv6;
+
+    let mut is_listening_ipv4 = false;
+    let mut is_listening_ipv6 = false;
+
+    if listener_ipv4.is_ok() {
+        is_listening_ipv4 = true;
+        event!(Level::INFO, "Listening on: {}", addr_ipv4);
+    } else {
+        event!(Level::WARN, "Failed to bind to ipv4: {}", addr_ipv4);
+    }
+
+    if listener_ipv6.is_ok() {
+        is_listening_ipv6 = true;
+        event!(Level::INFO, "Listening on: {}", addr_ipv6);
+    } else {
+        event!(Level::WARN, "Failed to bind to ipv6: {}", addr_ipv6);
+    }
+
+    if !is_listening_ipv4 && !is_listening_ipv6 {
+        event!(
+            Level::ERROR,
+            "Could not bind to {} or {}. Exiting",
+            addr_ipv6,
+            addr_ipv4
+        );
+        panic!();
+    }
+
+    //Handle ipv4 and ipv6 simultaneously and end if ctrl_c is run
+    //
+    //This looks and is a bit janky. Need to look into a way of specifying
+    //a set of tasks for a select fo listen to based on a condition instead
+    //of using 3 select macros. For now this will work
+    loop {
+        if is_listening_ipv4 && is_listening_ipv6 {
+            tokio::select! {
+            _ = signal::ctrl_c() => {
+                event!(Level::WARN, "Ctrl-C recieved, shutting down");
+                break;
+            }
+            Ok((stream, addr)) = listener_ipv4.as_ref().unwrap().accept() => {
+                tokio::spawn(handle_connection(state.clone(), stream, addr, tasks.clone()));
+            }
+            Ok((stream, addr)) = listener_ipv6.as_ref().unwrap().accept() => {
+                tokio::spawn(handle_connection(state.clone(), stream, addr, tasks.clone()));
+            }
+            }
+        } else if is_listening_ipv4 {
+            tokio::select! {
+            _ = signal::ctrl_c() => {
+                event!(Level::WARN, "Ctrl-C recieved, shutting down");
+                break;
+            }
+            Ok((stream, addr)) = listener_ipv4.as_ref().unwrap().accept() => {
+                tokio::spawn(handle_connection(state.clone(), stream, addr, tasks.clone()));
+            }
+            }
+        } else {
+            tokio::select! {
+            _ = signal::ctrl_c() => {
+                event!(Level::WARN, "Ctrl-C recieved, shutting down");
+                break;
+            }
+            Ok((stream, addr)) = listener_ipv6.as_ref().unwrap().accept() => {
+                tokio::spawn(handle_connection(state.clone(), stream, addr, tasks.clone()));
+            }
+            }
+        }
+    }
+
+    Ok(())
+}